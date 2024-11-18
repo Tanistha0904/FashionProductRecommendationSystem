ebd0d0dd972c86b071a0c8bd14ed2223216b23b2
Tanistha0904 <144806029+Tanistha0904@users.noreply.github.com>
2024-11-18 19:17:24 +0530

    Initial commit

---
+++ appp.py
@@ -0,0 +1,27 @@
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+import numpy as np
+from numpy.linalg import norm
+import os
+
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+#print(model.summary())
+def extract_features(img_path,model):
+    img=image.load_img(img_path, target_size=(224,224))
+    img_array = img_path, target_size = (224, 224)
+    expanded_img_array = np.expand_dims(img_array, axis=0)
+    preprocessed_img = preprocess_input(expanded_img_array)
+    result = model.predict(preprocessed_img).flatten()
+    normalized_result = result / norm(result)
+
+    return normalized_result
+print(os.listdir('images'))
